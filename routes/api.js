b7ab930769fde9ff59ec3eca5924d33b1c68d48b
Leandro Souza <leandrosabreu@gmail.com>
2017-10-04 06:01:35 -0300

    Pequenas correções

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-//var Product = require('../models/product');
 var Livro = require('../models/livro');
-
-//Product.methods(['get', 'put', 'post', 'delete']);
-//Product.register(router, '/products');
 
 Livro.methods(['get', 'put', 'post', 'delete']);
 Livro.register(router, '/livros');
