2bf1475217c3f326f87a693f229b2ef7b40f81fd
Leandro Souza <leandroades@gmail.com>
2017-11-20 10:01:44 -0200

    Added cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var cors = require('cors');
 var	mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/rest_test_db');
 
 var app = express();
+
+app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -12,4 +16,4 @@ app.use(bodyParser.json());
 app.use('/api', require('./routes/api'));
 
 app.listen(3000);
-console.log('API is running on port 3000');	
+console.log('API is running on port 3000');
